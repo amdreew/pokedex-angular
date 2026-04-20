f497e5c599aab89f2b13e5d3f7bea4193c38545d
mospino <ospino.u@gmail.com>
2026-04-20 14:57:59 -0500

    version 18 operativa

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,19 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RequestInterceptor } from './interceptors/request.interceptor';
 import { throwIfAlreadyLoaded } from './guards/module-import.guard';
 import { GraphQLModule } from './graphql.module';
 
-@NgModule({
-  declarations: [],
-  imports: [BrowserAnimationsModule, BrowserModule, GraphQLModule, HttpClientModule],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: RequestInterceptor,
-      multi: true,
-    },
-  ],
-})
+@NgModule({ declarations: [], imports: [BrowserAnimationsModule, BrowserModule, GraphQLModule], providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
--- src/app/core/interceptors/request.interceptor.ts
+++ src/app/core/interceptors/request.interceptor.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse,
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
